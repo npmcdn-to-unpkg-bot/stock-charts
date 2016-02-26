479f6960265454fe16b50fa914b425bfd3d8a2d6
kurra niranjan <kurraniranjan31@gmail.com>
2016-02-25 21:08:55 -0600

    Sidebar for document page.

--- docs/documentation.js
+++ docs/documentation.js
@@ -8,13 +8,16 @@ var parseDate = d3.time.format("%Y-%m-%d").parse
 require("stylesheets/stock-charts");
 
 var Nav = require("lib/navbar");
-
+var MainContainer = require("lib/main-container");
+var Sidebar = require("lib/sidebar");
 class ExamplesPage extends React.Component {
 	render() {
 
 			return (
 				<div>
 					<Nav />
+					<MainContainer>
+					</MainContainer>
 
 				</div>
 			);
---
+++ docs/lib/sidebar.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+var React = require('react');
+
+var Sidebar = Recat.createClass({
+	render() {
+		return (
+			<div className="col-sm-3 col-md-2 sidebar">{this.props.children}</div>
+		);
+	}
+});
+
+module.exports = Sidebar;
